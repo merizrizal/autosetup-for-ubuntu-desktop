7de645f7affecbf71fa2ef9e3f5ba04034b241eb
merizrizal <meriz.rizal@gmail.com>
2025-01-04 13:37:23 +0700

    Add scrip to configure bashrc and install application from external URL

--- setup.sh
+++ setup.sh
@@ -8,4 +8,9 @@ mkdir -p $ROOT_DIR/logs
 date=$(date "+%Y_%m_%d_%H_%M_%N")
 export ANSIBLE_LOG_PATH=$ROOT_DIR/logs/playbook_$date.log
 ansible-playbook ./playbook_install.yml -u $USER
-yq '.prefs_tmp_files' host_vars/localhost.yml | while read value; do echo $value | cat $(yq '.*') | dconf load /org/; done
+$HOME/.local/bin/yq '.prefs_tmp_files' host_vars/localhost.yml | while read value; do echo $value | cat $($HOME/.local/bin/yq '.*') | dconf load /org/; done
+
+echo "Will reboot in 7 seconds"
+echo "Press ctrl + c to abort"
+for i in {1..7}; do echo $i; sleep 1; done
+sudo reboot
